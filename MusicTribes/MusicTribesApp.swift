11f76acc662ce322832c1da5dfdbbcb561795ed6
federico lagarmilla <flagarmilla@ext.laligatech.com>
2024-05-16 23:34:12 -0300

    Initial Commit

---
+++ MusicTribes/MusicTribesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTribesApp.swift
+//  MusicTribes
+//
+//  Created by Federico Lagarmilla on 16/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicTribesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
